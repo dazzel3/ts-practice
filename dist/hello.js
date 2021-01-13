9ec5555782400ec056d5017b1a3ae21b0defc1ab
dazzel3 <aakk9350@naver.com>
2021-01-14 00:38:19 +0900

    [환경설정] ts 소개 및 컴파일러

---
+++ dist/hello.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var util_1 = require("./util");
+var value = util_1.default(1, 2);
+console.log(value);
---
+++ src/hello.ts
@@ -0,0 +1,3 @@
+import add from './util';
+const value = add(1,2);
+console.log(value);
